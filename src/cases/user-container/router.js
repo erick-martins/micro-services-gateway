c9fa6b7a01d4f83669787c2c3dd656254dd938a7
thiago jose de almeida simao <thiago.jsimao.me@gmail.com>
2018-11-11 23:43:22 -0200

    update the route test

--- src/cases/user-container/router.js
+++ src/cases/user-container/router.js
@@ -3,6 +3,8 @@ const { Router } = require("express");
 
 const router = Router();
 
-router.post("/user", createUser);
+router.post("/teste", (req, res) => {
+  res.json({ message: "esta funcionando a api" });
+});
 
 module.exports = router;
