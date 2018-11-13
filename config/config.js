f68e167b34d47b78bf26f44fca4b741e4389dca3
thiago jose de almeida simao <thiago.jsimao.me@gmail.com>
2018-11-12 23:58:44 -0200

    update the port project

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 const config = {
-  port: 3001,
+  port: 3000,
   superSecret: "EasyBaberToken",
   database:
     "mongodb://Aplic.EasyBarber:Aplic.EasyBarber%40Thi61183981@144.202.39.92:27017/EasyBarberDb"
