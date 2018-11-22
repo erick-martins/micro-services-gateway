3fec68453a4352483a89b25996f5320cf3645bec
Erick Martins <ericknmp@gmail.com>
2018-11-22 07:57:23 -0200

    Env var fix

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ const config = {
   port: process.env.PORT || 3000,
   superSecret: "EasyBaberToken",
   database: "mongodb://Aplic.EasyBarber:Aplic.EasyBarber%40Thi61183981@144.202.39.92:27017/EasyBarberDb"
-  //"mongodb://proc.imporSorl:M0SQTAFXCE4hiv@mongodb.dev.ltm.grupoltm.com.br:27017/solr?readPreference=primary"
+  mongodb: "mongodb://proc.imporSorl:M0SQTAFXCE4hiv@mongodb.dev.ltm.grupoltm.com.br:27017/solr?readPreference=primary"
 };
 
 module.exports = config;
