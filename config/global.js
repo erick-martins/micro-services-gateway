924e095892ec00fb1c5e74ca770f0a47a745de46
Erick Martins <ericknmp@gmail.com>
2018-12-01 14:30:15 -0200

    Initial Commit

--- config/global.js
+++ config/global.js
@@ -1,7 +1,6 @@
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const router = require("../src/cases/user-container/router");
-const authenticatedRoute = require("../src/midlewares/auth");
 
 global.Promise = require("bluebird");
 
@@ -9,8 +8,10 @@ module.exports = app => {
   app.use(morgan("dev"));
   app.set("json spaces", 4);
   app.use(bodyParser.json());
-  app.use(bodyParser.urlencoded({ extended: true }));
-  app.use(function(req, res, next) {
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
       "Access-Control-Allow-Headers",
@@ -18,6 +19,5 @@ module.exports = app => {
     );
     next();
   });
-  app.use("/api", authenticatedRoute);
   app.use(router);
 };
